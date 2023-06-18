25316eaa2266692b4203aef0e931ffb1f830539f
Marcos311 <marcosvinisabino@gmail.com>
2023-06-17 21:09:02 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Hello, World!";
+const texto2 = 'Hello, World!';
+const senha = "senhaSuperSegura123!";
+
+const citacao = "Meu nome é ";
+const meuNome = "Marcos";
+
+//console.log(citacao);
+
+//concatenação (+)
+console.log(citacao + meuNome);
+
+//template string ou template literal
