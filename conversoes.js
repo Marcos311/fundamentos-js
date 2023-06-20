d013e30fec8c9045e89b30e45b7e9dbee3ce4df1
Marcos311 <marcosvinisabino@gmail.com>
2023-06-20 13:14:52 -0300

    var codes and conversions

---
+++ conversoes.js
@@ -0,0 +1,17 @@
+// tipo de dado
+// booleans
+
+// conversão implícita
+const numero = 456;
+const numeroString = "456F";
+
+//Number()
+//String() 
+
+//console.log(numero === numeroString);
+
+console.log(numero + Number(numeroString));
+
+// conversão explícita
+
+
